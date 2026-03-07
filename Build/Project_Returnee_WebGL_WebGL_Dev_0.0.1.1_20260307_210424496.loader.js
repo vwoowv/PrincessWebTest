11cd010256dc19b042f8bd880b884aea24f9b772
vwoowv <vwoowv@dottwo.io>
2026-03-07 21:20:45 +0900

    [Build] 빌드 업데이트

--- Build/Project_Returnee_WebGL_WebGL_Dev_0.0.1.1_20260307_210424496.loader.js
+++ Build/Project_Returnee_WebGL_WebGL_Dev_0.0.1.1_20260307_210424496.loader.js
@@ -52,7 +52,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 194281410,
+    wasmFileSize: 194281865,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
