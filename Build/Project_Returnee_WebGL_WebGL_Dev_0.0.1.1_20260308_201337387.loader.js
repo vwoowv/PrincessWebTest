cec6ce03479816c188bcf82b33ea60bb09a32381
vwoowv <vwoowv@dottwo.io>
2026-03-08 20:18:57 +0900

    [Login] 로그인 확인을 위한 로그 추가

--- Build/Project_Returnee_WebGL_WebGL_Dev_0.0.1.1_20260308_201337387.loader.js
+++ Build/Project_Returnee_WebGL_WebGL_Dev_0.0.1.1_20260308_201337387.loader.js
@@ -52,7 +52,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 194268612,
+    wasmFileSize: 194270624,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
