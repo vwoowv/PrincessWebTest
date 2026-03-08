dafcef84ba68022f8fcf7e638160f0f9daa5c49c
vwoowv <vwoowv@dottwo.io>
2026-03-08 18:58:44 +0900

    [Login] 서버 로그인 과정 정리

--- Build/Project_Returnee_WebGL_WebGL_Dev_0.0.1.1_20260308_185241157.loader.js
+++ Build/Project_Returnee_WebGL_WebGL_Dev_0.0.1.1_20260308_185241157.loader.js
@@ -52,7 +52,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 194269747,
+    wasmFileSize: 194268012,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
