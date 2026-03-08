9961ae1c00cd6e0af82d7c2c839920f202eef1e9
vwoowv <vwoowv@dottwo.io>
2026-03-08 19:27:11 +0900

    [Server] 서버 연결 로그 추가

--- Build/Project_Returnee_WebGL_WebGL_Dev_0.0.1.1_20260308_192147913.loader.js
+++ Build/Project_Returnee_WebGL_WebGL_Dev_0.0.1.1_20260308_192147913.loader.js
@@ -52,7 +52,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 194268012,
+    wasmFileSize: 194268612,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
